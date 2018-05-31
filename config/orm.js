37889a0b874db252a6c271c807d111743246ceb1
Eliot Rodriguez <mr.ear84@gmail.com>
2018-05-31 17:01:27 -0400

    shows database info on webpage

--- config/orm.js
+++ config/orm.js
@@ -9,6 +9,14 @@ var orm = {
 			if(err) throw err;
 			callback(result);
 		})
+	},
+
+	update: function(tableInput, condition, callback){
+		var queryString = 'UPDATE ' + tableInput + 'SET devoured=true WHERE id=' + condition;
+		connection.query(queryString, function(err, result){
+			if(err)throw err;
+			callback(result);
+		})
 	}
 }
 
